4d36b0c070fab232c0064094128a552c058cd265
franksalas <frank.salas@gmail.com>
2019-02-13 13:02:39 -0600

    herewegoagain

---
+++ dash_tutorials/dash-tutorial.py
@@ -0,0 +1,16 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1'(Dash tutorials'),
+    dcc.Graph(id='example',
+                    figure = {
+                        'data': [{'x': [1,2,3,4,5], 'y':[5,6,7,2,1], 'type':line, 'name': 'boats']})
+    ])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
